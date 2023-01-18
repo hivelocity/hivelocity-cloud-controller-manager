8dabf296fa384d19ce6acfbf9c8b68590e688f6b
Thomas Guettler <thomas.guettler@syself.com>
2023-01-18 13:20:52 +0100

    use errors.As() (not type assertion).

--- client/api.go
+++ client/api.go
@@ -43,29 +43,34 @@ var ErrNoSuchDevice = errors.New("no such device")
 func (api *RealAPI) GetBareMetalDevice(ctx context.Context, deviceId int32) (*hv.BareMetalDevice, error) {
 	device, response, err := api.Client.BareMetalDevicesApi.GetBareMetalDeviceIdResource(
 		ctx, deviceId, nil)
-	if err != nil {
-		err, ok := err.(hv.GenericSwaggerError)
-		if !ok {
-			return nil, fmt.Errorf(
-				"unknown error during GetBareMetalDeviceIdResource StatusCode %d deviceId %q. %w",
-				response.StatusCode, deviceId, err)
-		}
-		var result struct {
-			Code    int
-			Message string
-		}
-		if err2 := json.Unmarshal(err.Body(), &result); err2 != nil {
-			return nil, fmt.Errorf(
-				"GetBareMetalDeviceIdResource failed to parse response body %s. StatusCode %d deviceId %q. %w",
-				err.Body(),
-				response.StatusCode, deviceId, err2)
-		}
+	if err == nil {
+		return &device, nil
+	}
 
-		if result.Message == "Device not found" {
-			return nil, ErrNoSuchDevice
-		}
-		return nil, fmt.Errorf("GetBareMetalDeviceIdResource failed with %d. deviceId %q. %w",
+	// something went wrong
+
+	var swaggerErr *hv.GenericSwaggerError
+	if !errors.As(err, swaggerErr) {
+		return nil, fmt.Errorf(
+			"unknown error during GetBareMetalDeviceIdResource StatusCode %d deviceId %q. %w",
 			response.StatusCode, deviceId, err)
 	}
-	return &device, nil
+	var result struct {
+		Code    int
+		Message string
+	}
+
+	if unmarshalErr := json.Unmarshal(swaggerErr.Body(), &result); unmarshalErr != nil {
+		return nil, fmt.Errorf(
+			"GetBareMetalDeviceIdResource failed to parse response body %s. StatusCode %d deviceId %q. %w",
+			swaggerErr.Body(),
+			response.StatusCode, deviceId, unmarshalErr)
+	}
+
+	if result.Message == "Device not found" {
+		return nil, ErrNoSuchDevice
+	}
+
+	return nil, fmt.Errorf("GetBareMetalDeviceIdResource failed with %d. deviceId %q. %w",
+		response.StatusCode, deviceId, err)
 }
